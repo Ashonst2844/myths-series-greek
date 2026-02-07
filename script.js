8c9e031bdd89da94337335f35dba07196be8f7e6
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-02-07 18:22:48 +0700

    fill Titan Page

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ navIndex("main");
 fetch('data/olympian-data.json')
     .then(response => response.json())
     .then(data => {
-        const container = document.querySelector('.char-detail');
+        const container = document.querySelector('#olympian-char');
         data.forEach((item) => {
             const charDiv = document.createElement('div');
             const lineBreak = document.createElement('hr');
@@ -25,6 +25,34 @@ fetch('data/olympian-data.json')
                 <p><img src="${item.image}" alt="${item.title}">${item.caption}</p>
             `;
             container.appendChild(charDiv);
+            container.appendChild(lineBreak);
+        });
+    })
+    .catch(error => console.error('Error loading JSON data:', error));
+
+fetch('data/titan-data.json')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.querySelector('#titan-char');
+        data.forEach((item) => {
+            const charDiv = document.createElement('div');
+            const lineBreak = document.createElement('hr');
+            lineBreak.style.border = "1px solid var(--text)";
+            lineBreak.style.marginBlock = "20px";
+            charDiv.classList.add('detail', 'reveal');
+            charDiv.setAttribute("id", item.id);
+            charDiv.innerHTML = `
+                <h2>${item.title}</h2><span>${item.role}</span>
+                <br>
+                <p>${item.description}</p>
+                <br>
+                <br>
+                <br>
+                <span>Pasangan:</span> ${item.lover}<br>
+                <span>Anak-anak:</span> ${item.child.join(', ')}
+            `;
+            container.appendChild(charDiv);
+            container.appendChild(document.createElement('br'));
             container.appendChild(lineBreak);
         });
     })
