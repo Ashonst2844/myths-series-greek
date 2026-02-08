36bc86216b5e756d680a3db67ef5036eabf34027
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-02-08 15:32:47 +0700

    fix error

--- script.js
+++ script.js
@@ -13,20 +13,12 @@ fetch('data/olympian-data.json')
     .then(response => response.json())
     .then(data => {
         const container = document.querySelector('#olympian-char');
-<<<<<<< HEAD
-        const list = document.querySelector('#olympian-list');
-        data.forEach((item) => {
-            const charDiv = document.createElement('div');
-            const lineBreak = document.createElement('hr');
-            const navLink = document.createElement('a');
-=======
         const list = document.querySelector("#olympian-list");
         data.forEach((item) => {
             const charDiv = document.createElement('div');
             const lineBreak = document.createElement('hr');
             const linkNav = document.createElement("a");
 
->>>>>>> b08379e6a0728d353d2cc67f16ccfbe93c815ebb
             lineBreak.style.border = "1px solid var(--text)";
             lineBreak.style.marginBlock = "20px";
             charDiv.classList.add('detail', 'reveal');
@@ -35,17 +27,10 @@ fetch('data/olympian-data.json')
                 <h2>${item.title}</h2><span>${item.description}</span>
                 <p><img src="${item.image}" alt="${item.title}">${item.caption}</p>
             `;
-<<<<<<< HEAD
-            navLink.setAttribute('href', '#'+item.id);
-            navLink.innerHTML = item.title;
-
-            list.appendChild(navLink);
-=======
             linkNav.setAttribute("href", "#"+item.id);
             linkNav.textContent = item.title;
 
             list.appendChild(linkNav);
->>>>>>> b08379e6a0728d353d2cc67f16ccfbe93c815ebb
             container.appendChild(charDiv);
             container.appendChild(lineBreak);
             container.appendChild(document.createElement("br"));
@@ -57,20 +42,12 @@ fetch('data/titan-data.json')
     .then(response => response.json())
     .then(data => {
         const container = document.querySelector('#titan-char');
-<<<<<<< HEAD
-        const list = document.querySelector('#titan-list');
-        data.forEach((item) => {
-            const charDiv = document.createElement('div');
-            const lineBreak = document.createElement('hr');
-            const navLink = document.createElement('a');
-=======
         const list = document.querySelector("#titan-list")
         data.forEach((item) => {
             const charDiv = document.createElement('div');
             const lineBreak = document.createElement('hr');
             const linkNav = document.createElement("a");
 
->>>>>>> b08379e6a0728d353d2cc67f16ccfbe93c815ebb
             lineBreak.style.border = "1px solid var(--text)";
             lineBreak.style.marginBlock = "20px";
             charDiv.classList.add('detail', 'reveal');
@@ -85,11 +62,10 @@ fetch('data/titan-data.json')
                 <span>Pasangan:</span> ${item.lover}<br>
                 <span>Anak-anak:</span> ${item.child.join(', ')}
             `;
-<<<<<<< HEAD
-            navLink.setAttribute('href', '#'+item.id);
-            navLink.innerHTML = item.title;
+            linkNav.setAttribute("href", "#"+item.id);
+            linkNav.textContent = item.title;
 
-            list.appendChild(navLink);
+            list.appendChild(linkNav);
             container.appendChild(charDiv);
             container.appendChild(document.createElement('br'));
             container.appendChild(lineBreak);
@@ -97,15 +73,16 @@ fetch('data/titan-data.json')
     })
     .catch(error => console.error('Error loading JSON data:', error));
 
-fetch('data/creature-data.json')
+    fetch('data/creature-data.json')
     .then(response => response.json())
     .then(data => {
         const container = document.querySelector('#creature-char');
-        const list = document.querySelector('#creature-list');
+        const list = document.querySelector("#creature-list");
         data.forEach((item) => {
             const charDiv = document.createElement('div');
             const lineBreak = document.createElement('hr');
-            const navLink = document.createElement('a');
+            const linkNav = document.createElement("a");
+
             lineBreak.style.border = "1px solid var(--text)";
             lineBreak.style.marginBlock = "20px";
             charDiv.classList.add('detail', 'reveal');
@@ -114,19 +91,13 @@ fetch('data/creature-data.json')
                 <h2>${item.title}</h2>
                 <p><img src="${item.image}" alt="${item.title}">${item.description}</p>
             `;
-            navLink.setAttribute('href', '#'+item.id);
-            navLink.innerHTML = item.title;
-
-            list.appendChild(navLink);
-=======
             linkNav.setAttribute("href", "#"+item.id);
             linkNav.textContent = item.title;
 
             list.appendChild(linkNav);
->>>>>>> b08379e6a0728d353d2cc67f16ccfbe93c815ebb
             container.appendChild(charDiv);
-            container.appendChild(document.createElement('br'));
             container.appendChild(lineBreak);
+            container.appendChild(document.createElement("br"));
         });
     })
     .catch(error => console.error('Error loading JSON data:', error));
